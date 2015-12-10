3ac69e067ab804ee2a222547fa7ebd5711cd3826
tedunderwood <tunder@illinois.edu>
2015-12-09 23:17:59 -0600

    Adding figure 1.

---
+++ plot/Figure1Histogram.R
@@ -0,0 +1,16 @@
+library(scales)
+library(ggplot2)
+
+stewacc <- scan('/Users/tunder/Dropbox/fiction/code/stewaccuracies.csv')
+randomacc <- scan('/Users/tunder/Dropbox/fiction/code/finalaccuracies.csv')
+detectiveacc <- scan('/Users/tunder/Dropbox/fiction/code/detaccuracies.csv')
+allacc <- data.frame(acc = c(randomacc, stewacc, detectiveacc),
+                     type = c(rep('\nrandom\n', 40), rep('genre\nfiction\n', 40), rep('\ndetective\n', 20)))
+
+p <- ggplot(allacc, aes(x = acc, colour = type, fill = type)) + geom_histogram(binwidth = 0.01) +
+  scale_colour_manual(values = c('black', 'black', 'black')) + scale_fill_manual(values = c('coral', 'gray50', 'dodgerblue')) +
+  theme(text = element_text(size = 16)) +
+  theme(legend.title=element_blank()) +
+  scale_y_continuous('count', breaks = c(2,4,6,8,10)) +
+  scale_x_continuous('\naccuracy with 140 positive and negative instances', limits = c(0.3,1), labels = percent, breaks = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9, 1))
+plot(p)
