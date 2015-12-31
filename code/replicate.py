5ba83a2bce09c0ede5e39926214029e3f1e00306
tedunderwood <tunder@illinois.edu>
2015-12-30 22:15:37 -0600

    Added gothic predict.

--- code/replicate.py
+++ code/replicate.py
@@ -332,6 +332,55 @@ def replicate_detective():
         for accuracy in accuracies:
             f.write(str(accuracy) + '\n')
 
+def project_gothic_beyond_date(dividedate):
+
+    print('First we create a model of gothic fiction only after ' + str(dividedate))
+
+    sizecap = 300
+
+    modelname = 'gothicjustpost' + str(dividedate)
+    paths = make_paths(modelname)
+    sourcefolder, extension, metadatapath, outputpath1, vocabpath = paths
+
+    exclusions = make_exclusions(dividedate, 2000, sizecap, 'nonegatives')
+
+    positive_tags = ['lochorror', 'pbgothic', 'locghost', 'stangothic', 'chihorror']
+    negative_tags = ['random', 'chirandom']
+    testconditions = set()
+
+    datetype = "firstpub"
+    numfeatures = 10000
+    regularization = .000075
+
+    classifyconditions = (positive_tags, negative_tags, datetype, numfeatures, regularization, testconditions)
+
+    rawaccuracy, allvolumes, coefficientuples = logisticpredict.create_model(paths, exclusions, classifyconditions)
+
+    print('If we divide the dataset with a horizontal line at 0.5, accuracy is: ', str(rawaccuracy))
+    print()
+    print('Then we create a model of gothic fiction blindly predicting after ' + str(dividedate))
+
+    modelname = 'gothicpredictpost' + str(dividedate)
+    paths = make_paths(modelname)
+    sourcefolder, extension, metadatapath, outputpath2, vocabpath = paths
+
+    exclusions = make_exclusions(0, 2001, sizecap, 'nonegatives')
+
+    testconditions = {'1700', str(dividedate)}
+
+    classifyconditions = (positive_tags, negative_tags, datetype, numfeatures, regularization, testconditions)
+
+    rawaccuracy, allvolumes, coefficientuples = logisticpredict.create_model(paths, exclusions, classifyconditions)
+
+    print('If we divide the second dataset at 0.5, accuracy is: ', str(rawaccuracy))
+    print()
+
+    # Now we compare the predictions made by these two models, comparing only
+    # the volumes that are in both models but excluded from the training process
+    # in the second model.
+
+    comparemodels.compare_untrained(outputpath1, outputpath2)
+
 if __name__ == '__main__':
 
     args = sys.argv
@@ -342,6 +391,7 @@ if __name__ == '__main__':
         print('  1) Extrapolate a model of LoC "detective" fiction to the Indiana exhibition.')
         print('  2) Extrapolate a model of detective fiction beyond a particular date.')
         print('  3) Extrapolate a model of one tag to another.')
+        print('  4) Extrapolate a model of gothic fiction beyond a particular date.')
 
         userchoice = int(input('\nyour choice: '))
 
@@ -352,6 +402,9 @@ if __name__ == '__main__':
             dividedate = int(input('date beyond which to project: '))
         elif userchoice == 3:
             command = 'extrapolate_tag'
+        elif userchoice == 4:
+            command = 'extrapolate_gothic_date'
+            dividedate = int(input('date beyond which to project: '))
 
     else:
         command = args[1]
@@ -366,6 +419,11 @@ if __name__ == '__main__':
         tagtoproject = input('tag to project: ')
         tagtarget = input('tag to model: ')
         project_tag_to_another(tagtoproject, tagtarget)
+
+    if command == 'extrapolate_gothic_date':
+        if dividedate == 0:
+            dividedate = int(input('date beyond which to project: '))
+        project_gothic_beyond_date(dividedate)
 
 
 
