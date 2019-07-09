52bb4adf207f084cac26d40864f411e91bf726ba
tedunderwood <tunder@illinois.edu>
2019-07-09 14:48:50 -0500

    streamlining converter and increasing processor count

--- bert/converter.py
+++ bert/converter.py
@@ -5,16 +5,11 @@ Written by Thilina Rajapakse
 https://github.com/ThilinaRajapakse/BERT_binary_text_classification/blob/master/converter.py
 '''
 
-import torch
-import pickle
-from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
-                              TensorDataset)
-from torch.nn import CrossEntropyLoss, MSELoss
+import pickle, sys
 
 from tqdm import tqdm, trange
 import os
 from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM, BertForSequenceClassification
-from pytorch_pretrained_bert.optimization import BertAdam, WarmupLinearSchedule
 
 from multiprocessing import Pool, cpu_count
 from tools import *
@@ -23,8 +18,6 @@ import convert_examples_to_features
 # OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
 # import logging
 # logging.basicConfig(level=logging.INFO)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # The input data dir. Should contain the .tsv files (or other data files) for the task.
 DATA_DIR = "data/"
@@ -35,7 +28,11 @@ DATA_DIR = "data/"
 BERT_MODEL = 'bert-base-uncased'
 
 # The name of the task to train.
-TASK_NAME = 'first'
+TASK_NAME = sys.argv[1]
+
+if TASK_NAME != 'sf':
+    print('error')
+    sys.exit(0)
 
 # The output directory where the model predictions and checkpoints will be written.
 OUTPUT_DIR = f'outputs/{TASK_NAME}/'
@@ -46,17 +43,10 @@ CACHE_DIR = 'cache/'
 # Sequences longer than this will be truncated, and sequences shorter than this will be padded.
 MAX_SEQ_LENGTH = 128
 
-TRAIN_BATCH_SIZE = 24
-EVAL_BATCH_SIZE = 8
-LEARNING_RATE = 2e-5
-NUM_TRAIN_EPOCHS = 1
 RANDOM_SEED = 42
-GRADIENT_ACCUMULATION_STEPS = 1
-WARMUP_PROPORTION = 0.1
 OUTPUT_MODE = 'classification'
 
 CONFIG_NAME = "config.json"
-WEIGHTS_NAME = "pytorch_model.bin"
 
 output_mode = OUTPUT_MODE
 
@@ -74,7 +64,7 @@ print('num_labels: ', num_labels)
 label_map = {label: i for i, label in enumerate(label_list)}
 train_examples_for_processing = [(example, label_map, MAX_SEQ_LENGTH, tokenizer, output_mode) for example in train_examples]
 
-process_count = 1
+process_count = 10
 if __name__ ==  '__main__':
     print(f'Preparing to convert {train_examples_len} examples..')
     print(f'Spawning {process_count} processes..')
